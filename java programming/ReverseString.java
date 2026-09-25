1b5804855f6a59c6bc639df4256f758085b5e666
Vinit-Pathare <pathare1712vinit@gmail.com>
2026-09-25 10:03:37 +0530

    added change for string program

---
+++ java programming/ReverseString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class ReverseString
+{
+    public static void main(String[] args) {
+        String str ;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter name");
+        str = sc.nextLine();
+        System.out.println(str);
+    }
+}
